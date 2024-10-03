c4d2651e7cfee4e7db5a6601e21e71bd55817beb
rrruko <rukokarasu@gmail.com>
2024-10-03 04:35:01 -0700

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module hydra-ogmios-test-client
 
-go 1.21.1
+go 1.20
+
+require (
+	github.com/SundaeSwap-finance/ogmigo/v6 v6.0.0-20240613041327-627a9f8c8240
+        github.com/SundaeSwap-finance/ogmigo v0.8.1 // indirect
+	github.com/aws/aws-sdk-go v1.44.197 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,103 @@
 package main
 
 import (
+        "context"
+        "encoding/json"
         "fmt"
+        "os"
+        "os/signal"
         
         "github.com/SundaeSwap-finance/ogmigo/v6"
 )
 
-func main() {
+type Transaction struct {
+        CborHex string
+        TxId    string
+}
+
+func parseTx(filepath string) (*Transaction, error) {
+        data, err := os.ReadFile(filepath)
+        if err != nil {
+                return nil, fmt.Errorf("failed to read file: %w", err)
+        }
+        var txEnvelope struct {
+                Transaction Transaction
+        }
+        err = json.Unmarshal(data, &txEnvelope)
+        if err != nil {
+                return nil, fmt.Errorf("failed to decode tx envelope: %w", err)
+        }
+        return &txEnvelope.Transaction, nil
+}
+
+func submitTx(endpoint string, filepath string) {
+        tx, err := parseTx(filepath)
+        ctx := context.Background()
         ogmigoClient := ogmigo.New(
-                ogmigo.WithEndpoint("0.0.0.0:9001"),
+                ogmigo.WithEndpoint(endpoint),
+        )
+        resp, err := ogmigoClient.SubmitTx(ctx, tx.CborHex)
+        if err != nil {
+                fmt.Printf("failed to submit tx: %w\n", err)
+                return
+        }
+        fmt.Printf("%s\n", resp.ID)
+}
+
+func doChainSync(endpoint string) {
+        ctx := context.Background()
+        ogmigoClient := ogmigo.New(
+                ogmigo.WithEndpoint(endpoint),
         )
         var callback ogmigo.ChainSyncFunc = func(ctx context.Context, data []byte) error {
-                fmt.Println("Received chainsync msg: %s", string(data))
+                
+                fmt.Printf("Received chainsync msg: %s\n", string(data))
+                return nil
 	}
 	chainSync, err := ogmigoClient.ChainSync(ctx, callback,
 		ogmigo.WithReconnect(true),
 	)
 	if err != nil {
-		return err
+		fmt.Println("%w", err)
 	}
-	defer chainSync.Close()
+	
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Kill, os.Interrupt)
+
+	select {
+	case <-chainSync.Done():
+		fmt.Println("chainsync done")
+	case <-ctx.Done():
+		fmt.Println("context done")
+	case <-stop:
+		fmt.Println("caught SIGINT")
+	}
+        errs := chainSync.Close()
+        fmt.Printf("chainsync errs: %v\n", errs)
+}
+
+func main() {
+        if len(os.Args) < 2 {
+                fmt.Printf("no action supplied\n")
+                return
+        }
+        if len(os.Args) < 3 {
+                fmt.Printf("no endpoint supplied\n")
+                return
+        }
+        args := os.Args[1:]
+        action := args[0]
+        endpoint := args[1]
+        if action == "chainsync" {
+                doChainSync(endpoint)
+        } else if action == "submit-tx" {
+                if len(args) < 3 {
+                         fmt.Printf("no filepath supplied\n")
+                         return
+                }
+                filepath := args[2]
+                submitTx(endpoint, filepath) 
+        } else {
+                fmt.Printf("unexpected action: %w\n", action)
+        }
 }
